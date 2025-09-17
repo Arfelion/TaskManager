c65f912ada7f4ac38436922019e1226999be8029
amgolubenny <amgolubenny@gmail.com>
2025-09-17 19:33:17 +0300

    First Commit

---
+++ src/main/java/skillclan/taskmanager/controller/TaskController.java
@@ -0,0 +1,16 @@
+package skillclan.taskmanager.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class TaskController {
+    @GetMapping("/")
+    public ResponseEntity hello() {
+        return ResponseEntity.ok("Тут може бути ваша реклама - Hello");
+    }
+}
